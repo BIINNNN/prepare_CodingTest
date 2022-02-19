e1b605fd23aaab5364c630dd524a00e6a9faa256
BIINNNN <seobin99@gmail.com>
2022-02-19 19:32:26 +0900

    python-for-coding : chapter 11 곱하기 혹은 더하기

---
+++ Python3/python-for-codingtest/greedy/ch11-2.py
@@ -0,0 +1,14 @@
+s = input()
+
+result = int(s[0])
+
+for i in range(1, len(s)):
+    num = int(s[i])
+    
+    if num <= 1 or result <= 0: # 1혹은 0이 연산에 포함되는 경우
+        result += num
+    else:
+        result *= num
+
+print(result)
+    
