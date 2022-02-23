425028e04e3c7e33d979696248327e7822a1df16
BIINNNN <seobin99@gmail.com>
2022-02-23 13:53:51 +0900

    python-for-coding-test : chapter 11 곱하기 혹은 더하기(두 번째 풀이 중 기존 코드 오류 발견)

--- Python3/python-for-codingtest/greedy/ch11-2.py
+++ Python3/python-for-codingtest/greedy/ch11-2.py
@@ -5,10 +5,9 @@ result = int(s[0])
 for i in range(1, len(s)):
     num = int(s[i])
     
-    if num <= 1 or result <= 0: # 1혹은 0이 연산에 포함되는 경우
+    if num <= 1 or result <= 1: # 1혹은 0이 연산에 포함되는 경우
         result += num
     else:
         result *= num
 
 print(result)
-    
